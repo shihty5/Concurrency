c65075a2b9684f0b5be2c665c9643d5a688e905b
Peter <shihty5@163.com>
2018-03-19 22:28:20 +0800

    Bad example(a++ is not thread safe)

---
+++ src/main/java/com/peter/concurrency/badexample/ConcurrencyTest.java
@@ -0,0 +1,48 @@
+package com.peter.concurrency.badexample;
+
+import com.peter.concurrency.annotations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@NotThreadSafe
+public class ConcurrencyTest {
+
+    //请求总数
+    public static int clientTotal = 5000;
+
+    //同时并发数
+    public static int threadTotal = 200;
+
+    public static int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService pool = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for (int i = 0; i < clientTotal; i++) {
+            pool.execute(() -> {
+                try {
+                    semaphore.acquire();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                countDownLatch.countDown();
+                add();
+                semaphore.release();
+            });
+        }
+        countDownLatch.await();
+        pool.shutdown();
+        System.out.println("count: " + count);
+    }
+
+    private static void add() {
+        count++;
+    }
+}
